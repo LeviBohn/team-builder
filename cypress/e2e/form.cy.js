c0aed8c7257f052edb437e46cc324340f214b502
Levi Bohn <levibohn@gmail.com>
2023-04-08 16:46:12 -0500

    successfully wrote some input testing; tests for terms checked, data submitted, and left empty are not yet written successfully.

--- cypress/e2e/form.cy.js
+++ cypress/e2e/form.cy.js
@@ -1,24 +1,39 @@
 //Get the Name input and type a name in it.
-describe('Get Name', function() {
-    it('Get the Name input and type a name in it', function() {
-        //Use an assertion to check if the text inputted contains the name you provided (Hint: use the .should assertion)
-        cy.get('input[name=Name]').should('contain', 'Levi')
-    })
-})
-
 //Get the Email input and type an email address in it
-describe('Get Email', function() {
-    it('Get the Email input and type an email address in it', function() {
-        cy.get('input[email=email]').should('contain', 'Levi@email.com')
+//Get the password input and type a password in it
+describe('Get Inputs and type something in each', function() {
+    beforeEach(() => {
+        // Each test needs fresh state
+        // Tests shouldn't rely on other tests
+        // Every test should work in isolation!
+
+        cy.visit("http://localhost:3000/");
+    })
+
+// Helpers (id GETTERS)
+const nameInput = () => cy.get("input[name=name]");
+const emailInput = () => cy.get("input[name=email]");
+const roleInput = () => cy.get("input[name=role]");
+
+    it('can type in the inputs', function() {
+        nameInput()
+            .should('have.value', '')
+            .type("Levi Bohn")
+            .should("have.value", "Levi Bohn");
+
+        emailInput()
+            .should('have.value', '')
+            .type("Levi@email.com")
+            .should("have.value", "Levi@email.com");
+
+        roleInput()
+            .should('have.value', '')
+            .type("student")
+            .should("have.value", "student");
     })
 })
 
-//Get the password input and type a password in it
-describe('Get Password', function() {
-    it('Get the password input and type a password in it', function() {
-        
-    })
-})
+
 
 //Set up a test that will check to see if a user can check the terms of service box
 describe('Terms can be Checked', function() {
